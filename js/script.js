b7a7558819bbe22831f29e6d7c2301cd02d4c3c2
Vlad <vladkushnir856@gmail.com>
2021-05-17 23:06:45 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 window.addEventListener('DOMContentLoaded', function() {
-    'use strict';
 
+    'use strict';
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
-    function hideTabContent(a){
+    function hideTabContent(a) {
         for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
@@ -14,24 +14,73 @@ window.addEventListener('DOMContentLoaded', function() {
 
     hideTabContent(1);
 
-    function showTabContent(b){
-        if (tabContent[b].classList.contains('hide')){
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('show');
-        }   
+        }
     }
 
     info.addEventListener('click', function(event) {
         let target = event.target;
-        if(target && target.classList.contains('info-header-tab')){
-           for (let i = 0; i < tab.length; i++) {
-               if(target == tab[i]) {
+        if (target && target.classList.contains('info-header-tab')) {
+            for(let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
                     hideTabContent(0);
                     showTabContent(i);
                     break;
-               }
-           } 
+                }
+            }
         }
 
     });
-});
+
+    // Timer 
+    let deadline = '2021-11-21';
+
+    function getTimeRemaining(endtime){
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes =  Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
+        
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+            
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            hours.textContent = (t.hours);
+            minutes.textContent = (t.minutes);
+            seconds.textContent = (t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('timer', deadline);
+
+
+    
+});    
+
+
+//Modal
+
+
+
+
